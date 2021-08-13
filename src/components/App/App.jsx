0648b8f26d5be57e78b5d58b01fdf83c1ac199b3
samson vang <vangsam2@gmail.com>
2021-08-13 13:14:27 -0500

    Setup for GalleryItem /Buttons/Images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryItem from '../GalleryItem/GalleryItem';
+
 
 function App() {
+
+  // useEffect() {
+  //   fetchList;
+  // }
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-        <button>Like</button>
-        <img src ="images/lake.jpg" width="150" height="150"/>
-        <button>Like</button>
-        <img src="images/sunset.jpg" width="150" height="150"/>
-        <button>Like</button>
+        <GalleryItem />
+        
 
       </div>
     );
---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,23 @@
+
+
+function itemInGallery(){
+
+
+    return(
+        <>
+        <img src ="images/lake.jpg" width="150" height="150"/>
+            
+            <div>
+                <button>Like</button>
+            </div>
+            <p> 0 People like this</p>
+                <img src="images/sunset.jpg" width="150" height="150"/>
+            <div>
+                <button>Like</button>
+            </div>
+            <p> 0 People like this</p>
+
+        </>
+    )
+}
+export default itemInGallery;
